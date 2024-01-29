3b85a07e4cd1e9a9f8de8c6f987d495674e46ef0
D-973 <145328111+D-973@users.noreply.github.com>
2024-01-29 13:54:35 +0800

    Add files via upload

---
+++ ex 2 pt 2(direvisi)/index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const url = require('url');
+const members = require('./members.js');
+const users = require('./users.js');
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const method = req.method.toUpperCase();
+  const headers = req.headers;
+
+  if (trimmedPath === '') {
+    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(200);
+    res.end('This is the home page');
+  } else if (trimmedPath === 'about') {
+    const response = {
+      Status: 'success',
+      Message: 'response success',
+      Description: 'Exercise #03',
+      Date: new Date().toISOString(),
+      Data: members,
+    };
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify(response));
+  } else if (trimmedPath === 'users') {
+    const response = {
+        Data: users,
+      };
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(200);
+      res.end(JSON.stringify(response));
+  } else {
+    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(404);
+    res.end('Not found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
